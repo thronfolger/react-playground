57ae74a7b5a37e8d3fa3403818080684c1ebeb8d
thronfolger <git@example.com>
2019-05-09 12:50:55 +0300

    Удалены ненужные пакеты

--- src/index.js
+++ src/index.js
@@ -1,8 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import '../node_modules/normalize.css/normalize.css';
-import 'index.css';
+import './index.css';
 
 import ReactPlayground from 'main/ReactPlayground';
 
